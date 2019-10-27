435a73fe580d2e6f12225567ddec61ab2a8d9935
Korene Stuart <korene17@gmail.com>
2019-10-26 22:36:54 -0400

    changes

---
+++ VisualDemo/VisualDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VisualDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Demo!");
+            Console.ReadLine();
+        }
+    }
+}
